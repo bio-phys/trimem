4d0b457197c3bbb9f59108ca7d609fdc49a6afe1
Sebastian Kehl <sebastian.kehl@mpcdf.mpg.de>
2022-03-15 11:13:11 +0100

    Remove use of numpy's savetxt in XYZWriter.
    This fixes an issue with memory accumulation triggered from
    within numpy.lib.npyio.savetxt.

--- src/helfrich/mc/output/xyz.py
+++ src/helfrich/mc/output/xyz.py
@@ -4,6 +4,7 @@ This writes vertex coordinates only, i.e. the mesh is lost. It also writes
 data as plain text.
 """
 
+import io
 import pathlib
 import numpy as np
 from ._common import _create_part
@@ -21,13 +22,11 @@ class XyzWriter:
     def write_points_cells(self, points, cells):
         """Write points and ignore cells."""
 
-        rwflag = "w" if self.step_counter == 0 else "a"
+        rwflag = "w+" if self.step_counter == 0 else "a+"
         with self.fname.open(mode=rwflag) as fp:
-            np.savetxt(fp,
-                       points,
-                       fmt=["C\t%.6f", "%.6f", "%.6f"],
-                       header="{}\n#".format(len(points)),
-                       comments="",
-                       delimiter="\t")
+            fp.write("{}\n#\n".format(len(points)))
+            for row in points:
+                fmt = "\t".join(["C\t{: .6f}", "{: .6f}", "{: .6f}\n"])
+                fp.write(fmt.format(*row))
 
         self.step_counter += 1
