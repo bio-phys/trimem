547e79e08c973f5432e9d3a38c470ca3fc52df53
Sebastian Kehl <sebastian.kehl@mpcdf.mpg.de>
2022-04-20 14:47:47 +0200

    Move package setup to setup.cfg where possible.
    A minimal setup.py must remain as currently required by
    scikit-build. The idea is to move all the setup configuration
    to setup.cfg as soon as available from scikit-build.
    
    Fix missing declaration of dependencies on the fly.

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
-import sys
-
 from setuptools import find_packages
 from skbuild import setup
 
-
+# minimal version of setup() as still required by scikit-build for interception
 setup(
-    name="trimem",
-    version="0.0.1",
-    description='Evaluation of the Helfrich bending energy using OpenMesh',
-    author='Sebastian Kehl',
-    license="MIT",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    entry_points="""
-        [console_scripts]
-        mc_app=trimem.mc.app:cli
-    """,
+    entry_points={
+        'console_scripts': [
+            'mc_app=trimem.mc.app:cli',
+        ]
+    },
     cmake_install_dir="src/trimem",
     include_package_data=True,
 )
